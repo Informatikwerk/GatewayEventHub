670b4b55b7cb09516930adeec325cc547c6449d8
Jakub Rybinski <jakubrybinski@gmail.com>
2018-09-06 09:06:25 +0200

    removed unused redis config from app properties

--- src/main/java/de/informatikwerk/gatewayeventhub/config/ApplicationProperties.java
+++ src/main/java/de/informatikwerk/gatewayeventhub/config/ApplicationProperties.java
@@ -12,33 +12,5 @@ import java.time.Duration;
  */
 @ConfigurationProperties(prefix = "application", ignoreUnknownFields = false)
 public class ApplicationProperties {
-    private String jedisIp = "127.0.0.1";
-    private int jedisPort = 6379;
-    private int jedisMsgExpire = 30;
-
-
-    public String getJedisIp() {
-        return jedisIp;
-    }
-
-    public void setJedisIp(String jedisIp) {
-        this.jedisIp = jedisIp;
-    }
-
-    public int getJedisPort() {
-        return jedisPort;
-    }
-
-    public void setJedisPort(int jedisPort) {
-        this.jedisPort = jedisPort;
-    }
-
-    public int getJedisMsgExpire() {
-        return jedisMsgExpire;
-    }
-
-    public void setJedisMsgExpire(int jedisMsgExpire) {
-        this.jedisMsgExpire = jedisMsgExpire;
-    }
 
 }
